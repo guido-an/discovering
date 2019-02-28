4f9c2ce97fcdb638e141037ee94fb786658fd895
Guido <Guido@mac.local>
2019-02-28 19:44:41 +0100

    stop and restart time after checked answer

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ const myQuestions = [
 ]
 
 function showQuestion(array){
+    countScore()          // show score
     for(var i = 0; i < array.length; i++) {
       $("#myQuestion").html(myQuestions[0].question)  // show the first question in the array
       $("#tip").html(myQuestions[0].tip)              // show relative tip
@@ -39,40 +40,43 @@ function nextQuestion() {
       $("#userAnswerCatcher").val("")                     // reset input placeholder
       $('#score-title').css("color", "#4799d4")                 // reset "score" color
       $('input').attr('readonly', false);               // make possible to write again on "input"
-      
-     
+           
     } else {
       $('#main').css("display", "none")                  // display none all the game when questions are finished
     }
   }
 
-var score = 120;                                    // counting score
- var intervalId = setInterval(function() {
-   if (score >= 0) {
-     $('#score').html(score)     // show "score" value
-   } else {
-   
-     $('#game-main').css('display', 'none')    // when the time is over hide no the game
-     $('#final-score-time-over').css('display', 'block')   // display "time is over"
-     $('#footer-game p').css('position', 'absolute').css('bottom', '0px').css('right', '10px') // keep copyright at the bottom
-     clearInterval(intervalId);        
-   }
-   score--;
- }, 1000);
-     
+var score = 120;  
+var intervalId;
+function countScore() {        // counting score
+  intervalId = setInterval(function() {
+    if (score >= 0) {
+      $('.score').html(score)     // show "score" value
+    } else {
+      $('#game-main').css('display', 'none')    // when the time is over hide all the page
+      $('#final-score-time-over').css('display', 'block')   // display "time is over"
+      $('#footer-game p').css('position', 'absolute').css('bottom', '0px').css('right', '10px') // keep copyright at the bottom
+      clearInterval(intervalId);        
+    }
+    score--;
+  }, 1000);
+}
+
 
 function checkAnswer(){
-
+  clearInterval(intervalId);   
   let userAnswer   = $("#userAnswerCatcher").val(); 
   let answerResult = $("#answerCheck");
   $('#next-question').toggleClass('display')    // make "next question" appears
   changeNextQuestionWord()
+ 
 
      if(userAnswer == myQuestions[0].answer) {
         answerResult.html('<p id="correct-answer">Correct answer. +10 points</p>')
         $('#score-title').css("color", "green")  // it makes "score" green if the answer is correct 
         score += 10     
         $('input').attr('readonly', true);   // prevent from writing again in the input after the user check the answer
+        
      } else {
         $('input').attr('readonly', true);   // prevent from writing again in the input after the user check the answer
         answerResult.html(' <p id="wrong-answer">Wrong answer.</p> The correct answer is ' +  '"' + myQuestions[0].answer + '"')
@@ -81,19 +85,23 @@ function checkAnswer(){
 
  function showPhoto() {
    $('#myPhoto').css("background-image", 'url(' + myQuestions[0].photo + ')')
-   
  }
-
 
  function changeNextQuestionWord() {     // change "next question" and link 
   if(myQuestions.length == 1) {
     $('#next-question').html("CHECK THE RESULT")
-    $( "#next-question" ).attr( "href", "https://www.google.com" ); // when questions are over, send the user to the next page   
-  }
+    $( "#next-question" ).attr( "href", "finished.html" ); // when questions are over, send the user to the next page 
+  } 
  }
 
  function showHowToPlay(){
    $('#how-to-play-button').toggleClass('display')
+ }
+
+ function finalScoreCatcher() {
+   if($('#next-question').html() == "CHECK THE RESULT") {
+       
+   }
  }
  
 
