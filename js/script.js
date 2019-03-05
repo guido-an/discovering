7498a9555fa96a6233c50a06104617bbcf028f50
Guido <Guido@mac.local>
2019-03-05 20:49:44 +0100

    code updated from branch

--- js/script.js
+++ js/script.js
@@ -1,47 +1,61 @@
 $(document).ready(function () {
-
-
-  $('#start-play-button').click(() => {   // when click on start play 
-    $('#home').addClass('display');        // hide home
-    showPreCountDown();   
-    $('#pre-count-down-section').css('display', 'block')  // display the pre count down page
-    
-  })
-
-  showQuestion(myQuestions)
-  showPhoto()
-
-  $("#check").click(() => checkAnswer())            // Check if the answer is correct, return "correct" or "wrong"
   
-  $('#next-question').click(() => {
-    nextQuestion()   
-    countScore()                                 // When click on "next question", next question appears
-   $('#next-question').toggleClass('display')         // Set display none for "next quesiton" (when the user click on check answer "next question appears again")
-      if($('#next-question').html() == "CHECK THE RESULT") {
-        clearInterval(intervalId)                  // stop the counter in the last page 
-        $('#game-main').css('display', 'none')      // hide the game body when the user click on "check the result"
-        $('#final-score-section').css('display', 'block'); // display the final score page
-        $('#finalScore').html(finalScore);         // display the final score 
-        $('.footer').css('position', 'fixed').css('bottom', '0') // on final score page, it keeps the footer at the bottom
-        $('.footer p').css('color', 'white');
-      }
+  howToPlayButton     = $('#how-to-play-button')
+  startPlayButton     = $('#start-play-button')
+  howToPlaySection    = $('#how-to-play-section')
+  footerHome          = $('#footer-home')
+  preCountDownNumber  = $('#pre-count-down-number')
+  preCountDownSection = $('#pre-count-down-section')
+  gamePage            = $('#game')
+  gameMainSection     = $('#game-main')
+  homePage            = $('#home')
+  myQuestion          = $("#myQuestion")
+  myHint              = $("#tip")
+  myNumber            = $("#questionNumber")
+  myInput             = $('input')
+  answerResponse      = $("#answerCheck");
+  correctAnswer       = '<p id="correct-answer">Correct answer. +10 points</p>';
+  scoreUpdate         = $('.score').html(score) 
+  nextQuestion        = $('#next-question')
+  nextQuestionHtml    = $('#next-question').html()
+  myPhoto             = $('#myPhoto')
+  myPhotoDisplayNone  = myPhoto.css('background-image', 'none')
+  videoRamiMalek      = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/mP0VHJYFOAU?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+  nazareVideo         = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/Ftok14M5p8g?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+  homerSimpsonVideo   = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/SXyrYMxa-VI?autoplay=1" frameborder="0"; allow="autoplay"; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+  finalScorePage      = $('#final-score-section');
+
+  discoveringDom.howToPlay()
+  howToPlayButton.toggleClass('display')
+  discoveringDom.startPlay()
+  discoveringDom.showQuestion(myQuestions)
+
+
+  $("#check").click(() => discoveringDom.checkAnswer())            // Check if the answer is correct, return "correct" or "wrong"
+    nextQuestion.click(() => {
+      discoveringDom.showNextQuestion()   
+      countScore()                                 // When click on "next question", next question appears
+      nextQuestion.toggleClass('display')         // Set display none for "next quesiton" (when the user click on check answer "next question appears again")
+        if(nextQuestion.html() == "QUESTIONS ARE FINISHED, CHECK THE RESULT") {
+          clearInterval(intervalId)                  // stop the counter in the last page 
+          gameMainSection.css('display', 'none')      // hide the game body when the user click on "check the result"
+          finalScorePage.css('display', 'block'); // display the final score page
+          $('#finalScore').html(finalScore);         // display the final score 
+          $('.footer').css('position', 'fixed').css('bottom', '0') // on final score page, it keeps the footer at the bottom
+          $('.footer p').css('color', 'white');
+        }
  })
   
  $(document).keypress(function(e) {              
    var keycode = (e.keyCode ? e.keyCode : e.which);
    if (keycode == '13') {
-       checkAnswer()                                 // when user click "enter" check answer
+       discoveringDom.checkAnswer()                                 // when user click "enter" check answer
    }
   });
 
-  $('#how-to-play-button').click(() => {
-     $('#how-to-play-section').toggle("slow");      // show how to play rules 
-     $('#footer-home').css('position', 'fixed')     // keep footer at the bottom 
-  })
-
   $('#final-score-page-button').click(() => {
     $('#premium-version-section').toggle("slow");      // premium version coming soon appears 
-    $('input').attr('readonly', false);               // make the email input writable again 
+    myInput.attr('readonly', false);               // make the email input writable again 
  })
 
   AOS.init(); // animate plugin
