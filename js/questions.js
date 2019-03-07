81d846e186231bc3fefef3e739aa9c572bb2d999
Guido <Guido@mac.local>
2019-03-07 20:22:19 +0100

    code update

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+function Question(question, answer, photo, hint, number) {
+    this.question = question; 
+    this.answer = answer;
+    this.photo = photo;
+    this.hint = hint; 
+    this.number = number;    
+}
+ 
+var question1 = new Question (
+   "In which city this photo was taken?",
+   "Amsterdam",
+   '../img/jhon.jpg',
+    "Well, let's start easy...<br> It was the year 1969, in the middle of the Vietnam War. John Lennon and Yoko Ono decided to use their honeymoon to promote world peace.",
+    '1'
+    );
+var question2 = new Question (
+    "What's his name?",
+    "Rami Malek",
+    "",
+    "Recently won the best actor Oscar for his performance as Freddie Mercury in Bohemian Rhapsody.",
+    '2',
+    );
+var question3 = new Question (
+    "By what name this event passed into history?",
+    "Rumble in the Jungle",
+    '../img/ali.jpg',
+    "One of the greatest fights of all time.<br> Against all odds Muhammad Ali beats Big George Foreman (40 fights, 40 wins) and reinforces his position among the legends of the noble art.<br> That's the 1974 and we are in Zaire.",
+    '3',
+    );
+var question4 = new Question (
+    "What is the photographer's name?",
+    "Steve McCurry",
+    '../img/shaolin-monastery.jpg',
+    'Shaolin monks training in  Zhengzhou, China. This photo was taken by one of the most iconic voices in contemporary photography.',
+    '4',
+    );
+var question5 = new Question (
+   "Is it true or false?",
+    "true",
+    '../img/varanasi.jpg',
+    "We are in Varanasi, one of the most diverse, crazy, colourful and intense places of India: the place where many believers come to die and be cremated.<br><br> Around 300 bodies are burned every day along the Gange River.",
+    '5',
+    );
+
+var question6 = new Question (
+    "From which italian region are they from?",
+    "Sicily",
+    '../img/cannoli.jpg',
+    "Made of fried pastry dough and filled with delicious fresh ricotta, Cannoli are one of those things to absolutely try during a trip in Italy.",
+    '6',
+     );
+
+var question7 = new Question (
+    "How many meters was it?",
+    "24",
+    "",
+    "Welcome to Nazaré!<br> This spot in Portugal is well known for a reason: it produces the largest waves on planet Earth.<br> Recently the Brazilian surfer Rodrigo Koxa set a new record for the biggest wave ever surfed.",
+    '7',
+    );
+
+var question8 = new Question (
+    "Who is this character?",
+    "Homer Simpson",
+    '../img/quesiton-mark.jpg',
+    "And to finish with a toast, a wise man once said:<br> 'Here's to alcohol: the cause of, and solution to, all of life's problems'",
+    '8',
+    );
+
+
+var questionsArray = [question1, question2, question3, question4, question5, question6, question7, question8]
+
--- js/script.js
+++ js/script.js
@@ -1,63 +1,117 @@
 $(document).ready(function () {
   
-  howToPlayButton     = $('#how-to-play-button')
-  startPlayButton     = $('#start-play-button')
-  howToPlaySection    = $('#how-to-play-section')
-  footerHome          = $('#footer-home')
-  preCountDownNumber  = $('#pre-count-down-number')
-  preCountDownSection = $('#pre-count-down-section')
-  gamePage            = $('#game')
-  gameMainSection     = $('#game-main')
-  homePage            = $('#home')
-  myQuestion          = $("#myQuestion")
-  myHint              = $("#tip")
-  myNumber            = $("#questionNumber")
-  myInput             = $('input')
-  answerResponse      = $("#answerCheck");
-  correctAnswer       = '<p id="correct-answer">Correct answer. +10 points</p>';
-  scoreUpdate         = $('.score').html(score) 
-  nextQuestion        = $('#next-question')
-  nextQuestionHtml    = $('#next-question').html()
-  myPhoto             = $('#myPhoto')
-  myPhotoDisplayNone  = myPhoto.css('background-image', 'none')
+  answerResponse      = $("#answerCheck");    // when I remove 'var' it works 
+  correctAnswerText       = '<p id="correct-answer">Correct answer. +10 points</p>';
+  wrongAnswer         = "Wrong answer, the correct answer is " 
   videoRamiMalek      = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/mP0VHJYFOAU?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
   nazareVideo         = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/Ftok14M5p8g?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
   homerSimpsonVideo   = $('<iframe width="100%" height="650px" src="https://www.youtube.com/embed/SXyrYMxa-VI?autoplay=1" frameborder="0"; allow="autoplay"; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-  finalScorePage      = $('#final-score-section');
-
-  discoveringDom.howToPlay()
-  howToPlayButton.toggleClass('display')
-  discoveringDom.startPlay()
-  discoveringDom.showQuestion(myQuestions)
-
-
-  $("#check").click(() => discoveringDom.checkAnswer())            // Check if the answer is correct, return "correct" or "wrong"
-    nextQuestion.click(() => {
-      discoveringDom.showNextQuestion()   
-      countScore()                                 // When click on "next question", next question appears
-      nextQuestion.toggleClass('display')         // Set display none for "next quesiton" (when the user click on check answer "next question appears again")
-        if(nextQuestion.html() == "QUESTIONS ARE FINISHED, CHECK THE RESULT") {
-          clearInterval(intervalId)                  // stop the counter in the last page 
-          gameMainSection.css('display', 'none')      // hide the game body when the user click on "check the result"
-          finalScorePage.css('display', 'block'); // display the final score page
-          $('#finalScore').html(finalScore);         // display the final score 
-          $('.footer').css('position', 'fixed').css('bottom', '0') // on final score page, it keeps the footer at the bottom
-          $('.footer p').css('color', 'white');
-        }
- })
   
- $(document).keypress(function(e) {              
+   function showAllQuestionElements() {
+      $('#myQuestion').html(game1.printCurrentQuestion())  
+      $('#hint').html(game1.printCurrentHint())           
+      $('#question-number').html(game1.printCurrentNumber())  
+      game1.printCurrentPhoto()
+      ramiMalekQuestion()
+      nazareQuestion()
+   }
+   showAllQuestionElements()
+
+  
+$('#how-to-play-button').click(() => {          // when click on "how to play button" show the rules
+   $('#how-to-play-section').toggle("slow")    // show how to play rules 
+   $('#footer-home').css('position', 'fixed')  // keep footer at the bottom 
+})
+
+$('#start-play-button').click(()=> {                     // when click on "start play button"
+   $('#home').addClass('display-none');                 // hide homePage
+    $('#pre-count-down-section').removeClass('display-none')
+    $('#how-to-play-section').css('display', 'none')
+    showPreCountDown()
+})
+
+
+var preCountDown = 3;
+var myInterval;
+function showPreCountDown() {              // PRE COUNT DOWN
+  var myInterval = setInterval(function() {
+    if (preCountDown > 0) {
+      $('#pre-count-down-number').html(preCountDown);
+    } else {
+      $('#pre-count-down-number').html("Go!")
+         setTimeout(function(){ 
+      $('#pre-count-down-section') .css('display', 'none')   // hide "pre count down page"
+      $('#game').css('display', 'block')                     // and display "game page"
+    }, 1000);
+    clearInterval(myInterval); 
+  }
+  preCountDown--;
+}, 1000);
+  setTimeout(function(){ countScore() }, 4000);   // start counting score after 4 seconds (+ 1 second for the countdown to start )
+}
+
+function countScore() {        // COUNTING SCORE 
+   game1.intervalIdScore = setInterval(function() {
+     if (game1.score >= 0) {
+       $('.score').html(game1.score)     // show "score" value
+     } else {
+       $('#game-main').css('display', 'none')    // when the time is over hide all the page
+       $('#time-over').css('display', 'block')   // display "time is over"
+       $('.footer p').css("color", "white")      // on time over page, footer color white
+       $('#footer-game p').css('position', 'absolute').css('bottom', '0px').css('right', '10px') // keep copyright at the bottom
+       clearInterval(game1.intervalIdScore);        
+     }
+     game1.score--;
+   }, 1000);
+   }
+    
+   $(document).keypress(function(e) {              //check answer when press 'enter'
    var keycode = (e.keyCode ? e.keyCode : e.which);
    if (keycode == '13') {
-       discoveringDom.checkAnswer()                                 // when user click "enter" check answer
+      game1.checkAnswer()
+      $('input').attr('readonly', true); 
+      $('#answerCheck').removeClass('display-none')
+      $("#userAnswerCatcher").val("") 
    }
   });
+ 
+    $('#next-question').click(() => {
+         game1.nextQuestion()
+         showAllQuestionElements()
+         $('#next-question').css('display', 'none')
+         $('input').attr('readonly', false); 
+         $('#answerCheck').addClass('display-none')
+         $('#userAnswerCatcher').addClass('display-none')
+          countScore(); 
+          if(game1.questionsArray[game1.questionIndex].photo == "") {   // to show video
+              showVideo()
+          } else {
+             removeVideo()
+          }
+    });
 
-  $('#final-score-page-button').click(() => {
-    $('#premium-version-section').toggle("slow");      // premium version coming soon appears 
-    myInput.attr('readonly', false);               // make the email input writable again 
- })
+  function showVideo(video) {
+         $('#myPhoto').html(video).css('height', '650px') 
+   }
 
-  AOS.init(); // animate plugin
+  function removeVideo() {
+        $('#myPhoto').html("").css('height', '850px') // reset height defined in css
+  }
+   
+  function ramiMalekQuestion() {       
+    if (game1.questionIndex == 1) {
+       showVideo(videoRamiMalek)
+    }
+  }
+  function nazareQuestion() {       
+     if (game1.questionIndex == 6) {
+        showVideo(nazareVideo)
+     } 
+   }
 
-});
+$('#final-score-page-button').click(() => {
+   $('#premium-version-section').toggle("slow");      // premium version coming soon appears 
+})
+
+    
+  });
