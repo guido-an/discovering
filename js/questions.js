152e65fb9d1f2eb9fc15de24e4e7b7a819069faf
Guido <Guido@mac.local>
2019-03-07 20:47:55 +0100

    changed photo design

--- js/questions.js
+++ js/questions.js
@@ -9,7 +9,7 @@ function Question(question, answer, photo, hint, number) {
 var question1 = new Question (
    "In which city this photo was taken?",
    "Amsterdam",
-   '../img/jhon.jpg',
+   './img/jhon.jpg',
     "Well, let's start easy...<br> It was the year 1969, in the middle of the Vietnam War. John Lennon and Yoko Ono decided to use their honeymoon to promote world peace.",
     '1'
     );
@@ -23,21 +23,21 @@ var question2 = new Question (
 var question3 = new Question (
     "By what name this event passed into history?",
     "Rumble in the Jungle",
-    '../img/ali.jpg',
+    './img/ali.jpg',
     "One of the greatest fights of all time.<br> Against all odds Muhammad Ali beats Big George Foreman (40 fights, 40 wins) and reinforces his position among the legends of the noble art.<br> That's the 1974 and we are in Zaire.",
     '3',
     );
 var question4 = new Question (
     "What is the photographer's name?",
     "Steve McCurry",
-    '../img/shaolin-monastery.jpg',
+    './img/shaolin-monastery.jpg',
     'Shaolin monks training in  Zhengzhou, China. This photo was taken by one of the most iconic voices in contemporary photography.',
     '4',
     );
 var question5 = new Question (
    "Is it true or false?",
     "true",
-    '../img/varanasi.jpg',
+    './img/varanasi.jpg',
     "We are in Varanasi, one of the most diverse, crazy, colourful and intense places of India: the place where many believers come to die and be cremated.<br><br> Around 300 bodies are burned every day along the Gange River.",
     '5',
     );
@@ -45,7 +45,7 @@ var question5 = new Question (
 var question6 = new Question (
     "From which italian region are they from?",
     "Sicily",
-    '../img/cannoli.jpg',
+    './img/cannoli.jpg',
     "Made of fried pastry dough and filled with delicious fresh ricotta, Cannoli are one of those things to absolutely try during a trip in Italy.",
     '6',
      );
@@ -61,7 +61,7 @@ var question7 = new Question (
 var question8 = new Question (
     "Who is this character?",
     "Homer Simpson",
-    '../img/quesiton-mark.jpg',
+    './img/quesiton-mark.jpg',
     "And to finish with a toast, a wise man once said:<br> 'Here's to alcohol: the cause of, and solution to, all of life's problems'",
     '8',
     );
